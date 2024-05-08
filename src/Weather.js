3539b1aa31d674c5a5680a103d6ddb7adffe8ee8
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-05-08 19:04:49 +0100

    create project with axios - weather city

---
+++ src/Weather.js
@@ -0,0 +1,25 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
+
+export default function Weather({ city }) {
+  function handleResponse(response) {
+    alert(`Weather in ${response.data.name} ${response.data.main.temp}`);
+  }
+
+  let apiKey = `5863935ee9cca4c02ed68203f807c65b`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(handleResponse);
+
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="3"
+      color="blue"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
+}
